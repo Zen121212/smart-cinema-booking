da437b411a9de0f7a959935a11dd4dd08c3c31ae
Zein Kassem <zein.s.kassem@gmail.com>
2025-06-28 05:01:52 +0300

    routing+migration

--- smart-cinema-server/connection/connection.php
+++ smart-cinema-server/connection/connection.php
@@ -4,4 +4,4 @@ $db_user = 'root';
 $db_pass = 'root';
 $db_name   = 'smart_cinema_booking_db';
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
