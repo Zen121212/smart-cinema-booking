4cae299894dae8d117364c9575d87413e7aaba5e
Zein Kassem <zein.s.kassem@gmail.com>
2025-06-28 04:57:46 +0300

    feat(models): implement UserModel with user role specific method

---
+++ smart-cinema-server/models/User.php
@@ -0,0 +1,61 @@
+<?php
+require_once("Model.php");
+require_once("UserRole.php");
+
+class UserModel extends Model{
+    protected int $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    protected static string $table = "users";
+    public function __construct(array $data){
+        $this->id = $data["id"];
+        $this->name = $data["name"];
+        $this->email = $data["email"];
+        $this->password = $data["password"];
+    }
+    public function getId(): int {
+        return $this->id;
+    }
+    public function toArray(){
+        return [
+            "id"    => $this->id,
+            "name"  => $this->name,
+            "email" => $this->email
+        ];
+    }
+    public static function create(array $data) {
+        $findUser = static::find($data['email'], 'email');
+        if ($findUser !== null) {
+            return null;
+        }
+
+        if (isset($data['password'])) {
+            $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+        }
+
+        return parent::create($data);
+    }
+    public static function getUsersByRoleName(string $roleName) {
+        $sql = "
+            SELECT users.* 
+            FROM users
+            JOIN users_roles ON users.id = users_roles.user_id
+            JOIN roles ON users_roles.role_id = roles.id
+            WHERE roles.name = ?
+        ";
+        $query = static::$mysqli->prepare($sql);
+
+        $query->bind_param("s", $roleName);
+        $query->execute();
+
+        $result = $query->get_result();
+
+        $users = [];
+        while ($row = $result->fetch_assoc()) {
+            $users[] = new static($row);
+        }
+        return $users;
+    }
+
+}
