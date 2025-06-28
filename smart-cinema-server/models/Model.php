093087aacfcacc28c7e4f691e6d7fcd33a6d0e93
Zein Kassem <zein.s.kassem@gmail.com>
2025-06-28 04:59:16 +0300

    add generic methods

---
+++ smart-cinema-server/models/Model.php
@@ -0,0 +1,94 @@
+<?php 
+
+abstract class Model{
+    protected static string $table;
+    protected static string $primary_key = "id";
+    protected static ?mysqli $mysqli = null;
+
+    public static function setConnection(mysqli $mysqli) {
+        static::$mysqli = $mysqli;
+    }
+    private static function detectTypes($values) {
+        if (!is_array($values)) {
+            $values = [$values];
+        }
+        $types = '';
+        foreach ($values as $value) {
+            if (is_int($value)) {
+                $types .= 'i';
+            } 
+            else{
+                $types .= 's';
+            }
+        }
+        return $types;
+    }
+    public static function all(){
+        $sql = sprintf("Select * from %s", static::$table);
+        
+        $query = static::$mysqli->prepare($sql);
+        $query->execute();
+
+        $data = $query->get_result();
+
+        $objects = [];
+        while($row = $data->fetch_assoc()){
+            $objects[] = new static($row); //creating an object of type "static" / "parent" and adding the object to the array
+        }
+
+        return $objects; //we are returning an array of objects!!!!!!!!
+    }
+    public static function find($value, $key = null) {
+
+        $key = $key ?? static::$primary_key;
+
+        $sql = sprintf("SELECT * FROM %s WHERE %s = ? LIMIT 1", static::$table, $key);
+        $query = static::$mysqli->prepare($sql);
+
+        $type = static::detectTypes([$value]);
+
+        $query->bind_param($type, $value);
+        $query->execute();
+
+        $data = $query->get_result()->fetch_assoc();
+
+        return $data ? new static($data) : null;
+    }
+    public static function create(array $data) {
+        $columns = array_keys($data);
+        $placeholders = implode(', ', array_fill(0, count($columns), '?'));
+        $columnList = implode(', ', $columns);
+        $values = array_values($data);
+
+        $types = static::detectTypes($values);
+
+        $sql = sprintf(
+            "INSERT INTO %s (%s) VALUES (%s)",
+            static::$table,
+            $columnList,
+            $placeholders
+        );
+        $query = static::$mysqli->prepare($sql);
+
+        $query->bind_param($types, ...$values);
+        $query->execute();
+
+        $data['id'] = static::$mysqli->insert_id;
+        return new static($data);
+    }
+    public static function delete( int $id){
+        $sql = sprintf("Delete from %s WHERE %s = ?", 
+                        static::$table, 
+                        static::$primary_key);
+        $query = static::$mysqli->prepare($sql);
+
+        $query->bind_param("i", $id);
+        $query->execute();
+
+        return $query->affected_rows;
+    }
+
+}
+
+
+
