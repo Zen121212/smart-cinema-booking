c39e15ffa50f9f76656c73d6ac7066736320149a
Zein Kassem <zein.s.kassem@gmail.com>
2025-06-30 11:18:24 +0300

    feat(booking): add booking creation+index

---
+++ smart-cinema-server/migrations/003_create_users_roles_table.php
@@ -0,0 +1,14 @@
+<?php 
+require("../connection/connection.php");
+
+
+$query = "CREATE TABLE `users_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `users_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE)";
+
+$conn->query($query);
+$conn->close();
